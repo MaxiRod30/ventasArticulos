6ba28a28cc47c3793fdf5643be8992b7d5d5d5e6
MaxiRod30 <max.edw.rod@gmail.com>
2023-01-13 20:17:27 -0300

    Terminado JS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const baseDeDatos = [
         nombre: 'Contactor',
         descripcion: 'Contactor bobina 230vAC',
         marca: 'siemens',
-        precio: 1,
+        precio: 10,
         imagen: 'xxx.jpg'
     },
     {
@@ -12,7 +12,7 @@ const baseDeDatos = [
         nombre: 'Contactor',
         descripcion: 'Contactor bobina 230vAC',
         marca: 'abb',
-        precio: 1,
+        precio: 11,
         imagen: 'xxx.jpg'
     },
     {
@@ -20,7 +20,7 @@ const baseDeDatos = [
         nombre: 'Contactor',
         descripcion: 'Contactor bobina 230vAC',
         marca: 'schneider',
-        precio: 1,
+        precio: 12,
         imagen: 'xxx.jpg'
     },
     {
@@ -28,7 +28,7 @@ const baseDeDatos = [
         nombre: 'PLC',
         descripcion: 'PLC para control de procesos',
         marca: 'siemens',
-        precio: 1,
+        precio: 20,
         imagen: 'xxx.jpg'
     },
     {
@@ -36,7 +36,7 @@ const baseDeDatos = [
         nombre: 'PLC',
         descripcion: 'PLC para control de procesos',
         marca: 'abb',
-        precio: 1,
+        precio: 21,
         imagen: 'xxx.jpg'
     },
     {
@@ -44,7 +44,7 @@ const baseDeDatos = [
         nombre: 'PLC',
         descripcion: 'PLC para control de procesos',
         marca: 'schneider',
-        precio: 1,
+        precio: 22,
         imagen: 'xxx.jpg'
     },
     {
@@ -52,7 +52,7 @@ const baseDeDatos = [
         nombre: 'HMI',
         descripcion: 'interface hombre maquina',
         marca: 'siemens',
-        precio: 1,
+        precio: 30,
         imagen: 'xxx.jpg'
     },
     {
@@ -60,7 +60,7 @@ const baseDeDatos = [
         nombre: 'HMI',
         descripcion: 'interface hombre maquina',
         marca: 'abb',
-        precio: 1,
+        precio: 31,
         imagen: 'xxx.jpg'
     },
     {
@@ -68,7 +68,7 @@ const baseDeDatos = [
         nombre: 'HMI',
         descripcion: 'interface hombre maquina',
         marca: 'schneider',
-        precio: 1,
+        precio: 32,
         imagen: 'xxx.jpg'
     },
     {
@@ -76,7 +76,7 @@ const baseDeDatos = [
         nombre: 'rele termico',
         descripcion: 'rele termico para contactores',
         marca: 'siemens',
-        precio: 1,
+        precio: 40,
         imagen: 'xxx.jpg'
     },
     {
@@ -84,7 +84,7 @@ const baseDeDatos = [
         nombre: 'rele termico',
         descripcion: 'rele termico para contactores',
         marca: 'abb',
-        precio: 1,
+        precio: 41,
         imagen: 'xxx.jpg'
     },
     {
@@ -92,95 +92,185 @@ const baseDeDatos = [
         nombre: 'rele termico',
         descripcion: 'rele termico para contactores',
         marca: 'schneider',
-        precio: 1,
+        precio: 42,
         imagen: 'xxx.jpg'
     }
 
 ];
 let carritoCompras = [];
+let eleccion = "";
 
 function menuPrincipal() {
    let option =  prompt(`Bienvenido a ventas de articulos para la industria.
-
     Seleccione lo que desee comprar:
-
+    
+    0 - Salir
     1 - Contactores
     2 - PLCs
     3 - HMI
     4 - Reles termicos
-    5 - Salir
+
+    Otras opciones:
+
+    5 - Ver Compra
+    6 - Vaciar compra
+    7 - Finalizar compra
+    
     ` )
 
     return option;
 
 }
-
 function menuItem(Datos,filtro) {
     let msg = "0 - Salir"
     let obj=[]
-    let i = 1
+    let index = 1
 
     Datos.forEach(e => {
-        if(e.nombre === filtro){
+        if(e.nombre === filtro)
+        {
             obj.push(e)
-            if(msg != undefined){
-                msg = msg + "\n" +"            "+i +" - "+e.marca + " - Precio: "+e.precio}
-                i++
-        }     
+            msg = msg + "\n" +"            "+index +" - "+e.marca.toUpperCase() + " - Precio: "+e.precio;
+            index++;
+        }
     })
-
-    while (true) {
+   do{
         let option =  prompt(`
         Seleccione el modelo de ${filtro} que desee comprar:
+        
             ${msg}
         ` )
-        if (!isNaN(option) && parseInt(option)<i){
+        if (!isNaN(option) && parseInt(option)<index){
             return obj[parseInt(option)-1]
          }else{
             alert(`Escriba la opcion elegida mediante el numero al comienzo`)
          }
-    }
+    } while(option != "0")
 
  
  }
- function agregarCompras(option,filtro) {
-
-
+function agregarCompras(listaCompras,obj) {
+    // Agrego obejto al array
+    listaCompras.push(obj);
+    alert("Se agrego correctamente " + obj.nombre +" - "+ obj.marca)
 
  }
-let eleccion = menuPrincipal();
+function ListarCompras(listaCompras) {
+    //Listo el array de compras
+    let msg = "Lista de compras:"
+    let precioTotal = 0;
 
-if (!isNaN(eleccion)){
-    switch (parseInt(eleccion)) {
-        case 1:
-            //Mostrar menu de contactores
-            let opcionContactor = menuItem(baseDeDatos,"Contactor");
-            console.log(opcionContactor)
-            break;
-        case 2:
-            //Mostrar menu de PLC
-            let opcionPLC = menuItem(baseDeDatos,"PLC");
-            console.log(opcionPLC)
-            break;   
-        case 3:
-            //Mostrar menu de HMI
-            let opcionHMI = menuItem(baseDeDatos,"HMI");
-            console.log(opcionHMI)
-            break;
-        case 4:
-            //Mostrar menu de rele termico
-            let opcionRT = menuItem(baseDeDatos,"rele termico");
-            console.log(opcionRT)
-            break;
-        case 5:
-            
-            break;
+    for (let i = 0; i < listaCompras.length; i++) {
+            msg = msg + "\n" +i +" - "+ listaCompras[i].nombre.toUpperCase() + " - Precio: $"+ listaCompras[i].precio + " - Marca: " + listaCompras[i].marca.toUpperCase()
+            precioTotal = precioTotal + listaCompras[i].precio
+      }
+      msg = msg + "\n" + "Precio total = $ " + precioTotal 
+    alert(msg)
+    return precioTotal
+ }
+ function VaciarCompras() {
+    //Vaciar el array de compras
+    alert("Se borro correctamente la lista! ");
+    return [];
+ }
+ function FinalizarCompras(listaCompras) {
+    //Finalizar compra 
+    if (listaCompras != []){
+        let precioTotal = ListarCompras(listaCompras)   
+        let option = prompt(` Para terminar la compra escribir "OK", si desea cancelar escribir "CANCELAR"
 
-        default:
-            break;
+            Total a pagar: $ ${precioTotal}
+
+        `)
+        if(option == "OK"){
+            let nombreCliente = prompt(` Escriba su nombre para registrarlo en la compra ...`)
+            let domicilioCliente = prompt(` Escriba su domicilio para registrarlo en la compra ...`)
+            let numeroTelCliente = prompt(` Escriba su celular de contacto para registrarlo en la compra ...`)
+            alert(`Su compra se proceso con exito!
+
+                Nombre: ${nombreCliente}
+                Domicilio: ${domicilioCliente}
+                Celular: ${numeroTelCliente}
+                Total de la compra: $ ${precioTotal}
+
+                Muchas gracias por confiar en nosotros!
+                `)
+        }else if(option == "CANCELAR")
+        {
+            alert("Compra Cancelada!")
+        }else
+        {
+            alert(`Escriba una opcion "OK" o "CANCELAR" `)
+        }
+    }else{
+        alert("No tiene ningun elemento a comprar!")
     }
+ }
 
-}else
-{
-    alert(`Escriba la opcion elegida mediante el numero al comienzo`)
+ // -----------------------------------------
+while (eleccion != "0") {
+
+    //Mostrar menu principal
+    eleccion = menuPrincipal();
+    
+    if (!isNaN(eleccion)){
+        switch (parseInt(eleccion)) {
+            case 1:
+                //Mostrar menu de contactores
+                let opcionContactor = menuItem(baseDeDatos,"Contactor");
+                if (opcionContactor != undefined )
+                {
+                    agregarCompras(carritoCompras,opcionContactor)
+                }
+                
+                break;
+            case 2:
+                //Mostrar menu de PLC
+                let opcionPLC = menuItem(baseDeDatos,"PLC");
+                if (opcionPLC != undefined )
+                {
+                    agregarCompras(carritoCompras,opcionPLC)
+                }
+                break;   
+            case 3:
+                //Mostrar menu de HMI
+                let opcionHMI = menuItem(baseDeDatos,"HMI");
+                if (opcionHMI != undefined )
+                {
+                    agregarCompras(carritoCompras,opcionHMI)
+                }
+                
+                break;
+            case 4:
+                //Mostrar menu de rele termico
+                let opcionRT = menuItem(baseDeDatos,"rele termico");
+                if (opcionRT != undefined ){
+                    agregarCompras(carritoCompras,opcionRT)
+                }
+                break;
+            case 5:
+                //Mostrar menu de Ver Compra
+                ListarCompras(carritoCompras);
+                break;
+            case 6:
+                //Vaciar compra
+                carritoCompras = VaciarCompras(carritoCompras);
+            break;
+            case 7:
+                //Mostrar menu de Finalizar compra
+                FinalizarCompras(carritoCompras)
+            break;
+
+            default:
+                if (parseInt(eleccion)!= 0)
+                {
+                    alert(`Escriba la opcion elegida mediante el numero al comienzo`)
+                }
+            break;
+        }
+    
+    }else
+    {
+        alert(`Escriba la opcion elegida mediante el numero al comienzo`)
+    }
 }
